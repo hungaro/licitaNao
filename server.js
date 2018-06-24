699dc3efb0fa05c0929fe96f4176a249a04e4365
hungaro <fernandohungaro@gmail.com>
2018-06-24 17:24:43 -0300

    commit

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
- const app = express();
- 
- app.use(express.static(__dirname + '/dist/licitanao/'));
- app.listen(process.env.PORT || 8080);
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/licitanao'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);

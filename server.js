ddc1461a689cdeb0fb1fda80e6150361ae372dc2
hungaro <fernandohungaro@gmail.com>
2018-06-24 16:27:32 -0300

    commit

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
 const express = require('express');
-const http = require('http');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'dist/licitanao')))
-
-
-app.get('/*',(req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/licitanao/index.html'));
-})
-const port = process.env.PORT || 8080;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('Running'));
+ const app = express();
+ 
+ app.use(express.static(__dirname + '/dist'));
+ app.listen(process.env.PORT || 8080);

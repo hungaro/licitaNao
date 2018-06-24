febd00a2d4adc995af64bdd4a6dbb173311505bf
hungaro <fernandohungaro@gmail.com>
2018-06-24 17:40:38 -0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist/licitanao'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 5000);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, '/dist/licitaNao')))
+  .set('views', path.join(__dirname, 'index.html'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('/dist/licitaNao'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))

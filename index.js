6322016784a2747843ba9e861e8f42523b8ac171
hungaro <fernandohungaro@gmail.com>
2018-06-24 18:21:29 -0300

    commit

--- index.js
+++ index.js
@@ -4,7 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, '/dist/licitaNao')))
-  .set('views', path.join(__dirname, 'index.html'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('/dist/licitaNao'))
+  .get('/', (req, res) => res.render('/dist/licitaNao/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
